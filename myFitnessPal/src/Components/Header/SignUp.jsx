432657076b29195c7adc7a0f2d15bef9a11e3632
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2022-03-29 22:59:52 +0530

    sign up data is going to backend

--- myFitnessPal/src/Components/Header/SignUp.jsx
+++ myFitnessPal/src/Components/Header/SignUp.jsx
@@ -1,26 +1,70 @@
 import React from "react";
 import "./SignUp.css";
+import { useState } from "react";
+import axios from "axios"
 
 function SignUp() {
+  const [data, setData] = useState([]);
+  const [formData, setFormData] = useState({
+    "name": "",
+    "age": "",
+    "height": "",
+    "weight": "",
+    "email": "",
+    "password": "",
+  })
+
   const handleChange = (e) => {
-    /////
+    const { id, value } = e.target;
+    setFormData({...formData, [id]: value})
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/users", formData).then((res) => console.log(res)).catch((e)=> console.log(e))
+  }
+
   return (
-    <div id="signUp">
+    <div className="signUp">
       <h5>Almost there! Create your account</h5>
-      <form id="myForm">
-        <input id="name" placeholder="Name" onChange={handleChange} />
-        <br />
-        <input id="age" placeholder="Age" onChange={handleChange} />
-        <br />
-        <input id="hight" placeholder="Hight" onChange={handleChange} />
-        <br />
-        <input id="weight" placeholder="Weight" onChange={handleChange} />
-        <br />
-        <input id="email" placeholder="Email address" onChange={handleChange} />
+      <form className="myForm" onSubmit={handleSubmit}>
+        <input
+          className="name"
+          id="name"
+          placeholder="Name"
+          onChange={handleChange}
+        />
         <br />
         <input
+          className="age"
+          id="age"
+          placeholder="Age"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          className="height"
+          id="height"
+          placeholder="height (ft)"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          className="weight"
+          id="weight"
+          placeholder="Weight (lbs)"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          className="email"
+          id="email"
+          placeholder="Email address"
+          onChange={handleChange}
+        />
+        <br />
+        <input
+          className="password"
           id="password"
           placeholder="Create a password"
           onChange={handleChange}
@@ -30,20 +74,19 @@ function SignUp() {
           Must be at least 10 characters, no spaces.
         </label>
         <br />
-        <br />
         <input type={"checkbox"} />
-        <label id="label2">Terms & Conditions</label>
+        <label className="label2">Terms & Conditions</label>
         <br />
         <label className="label">
           I agree to MyFitnessPal's <span>Terms</span> & Conditions and{" "}
           <span>Privacy Policy</span>.
         </label>
         <br />
-        <input type="submit" value="CONTINUE" id="btn" />
+        <input type="submit" value="CONTINUE" className="btn" />
       </form>
       <label className="label">or</label>
       <br />
-      <button>CONTINUE WITH GOOLE</button>
+      <button>CONTINUE WITH GOOGLE</button>
       <br />
       <p>We will never post anything without your permission</p>
       <p>
